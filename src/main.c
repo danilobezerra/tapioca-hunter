21784a65dfbc48de7f9daed6804879918c264f3d
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2023-01-31 21:41:45 -0300

    some comments

--- src/main.c
+++ src/main.c
@@ -6,15 +6,21 @@ bool isRunning = TRUE;
 int main() {
     u16 idx = TILE_USER_INDEX;
 
+    // map setup
     PAL_setPalette(PAL0, game_pal.data, DMA);
     VDP_loadTileSet(&game_tileset, idx, DMA);
     Map* bga = MAP_create(&gamemap, BG_A, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx));
 
+    // game loop
     while (isRunning) {
+        // map drawing
         MAP_scrollTo(bga, 0, 0);
+
+        // wait vblank
         SYS_doVBlankProcess();
     }
 
+    // free map
     MEM_free(bga);
 
     return 0;
