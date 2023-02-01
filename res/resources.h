d546bca1144d674331384aa24c6a24f6ba5332b5
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2023-01-31 21:40:08 -0300

    Add tiled project, with .res setup

--- res/resources.h
+++ res/resources.h
@@ -1,5 +1,8 @@
 #ifndef _RES_RESOURCES_H_
 #define _RES_RESOURCES_H_
 
+extern const TileSet game_tileset;
+extern const Palette game_pal;
+extern const MapDefinition gamemap;
 
 #endif // _RES_RESOURCES_H_
---
+++ res/resources.res
@@ -0,0 +1,3 @@
+TILESET game_tileset "tiled/game_ts.tsx"
+PALETTE game_pal "tiled/tileset.png"
+MAP gamemap "tiled/map.tmx" mapdata
