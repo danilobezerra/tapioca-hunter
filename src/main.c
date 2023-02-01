9eb19bc406bff38481efd679d63b83c9b2eeec3a
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2023-01-31 21:40:47 -0300

    add testing map drawing

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
 #include <genesis.h>
-#include <resources.h>
+#include "resources.h"
 
 bool isRunning = TRUE;
 
 int main() {
-    VDP_drawText("Hello World!", 10, 13);
+    u16 idx = TILE_USER_INDEX;
+
+    PAL_setPalette(PAL0, game_pal.data, DMA);
+    VDP_loadTileSet(&game_tileset, idx, DMA);
+    Map* bga = MAP_create(&gamemap, BG_A, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx));
 
     while (isRunning) {
-
+        MAP_scrollTo(bga, 0, 0);
         SYS_doVBlankProcess();
     }
 
+    MEM_free(bga);
+
     return 0;
 }
+
