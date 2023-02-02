3f90256fe8dd9c1eaa8c272db1c27626c62ccf8d
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2023-02-02 01:57:15 -0300

    WIP tiled to collision data progress

---
+++ res/hitboxes.h
@@ -0,0 +1,14 @@
+#ifndef GGJ_2023_HITBOXES_GUARD
+#define GGJ_2023_HITBOXES_GUARD
+
+#include <genesis.h>
+
+static const Box hitboxes[] = {
+  { .x = 4, .y = 1, .w = 4, .h = 1 }, /* 1 */
+  { .x = 2, .y = 3, .w = 6, .h = 1 }, /* 2 */
+  { .x = 10, .y = 1, .w = 1, .h = 3 }, /* 3 */
+  { .x = 1, .y = 1, .w = 1, .h = 1 }, /* 4 */
+  { .x = 2, .y = 5, .w = 1, .h = 1 }, /* 5 */
+}
+
+#endif /* GGJ_2023_HITBOXES_GUARD */
---
+++ scripts/generate_hitboxes_code.lua
@@ -0,0 +1,160 @@
+--[[
+  This script is a port from `get_regions` method from
+  `csv_tilemap` code of "irene" game framework, by André Luiz Alvares.
+  https://github.com/Andre-LA/irene/blob/master/irene/utils/csv_tilemap.nelua
+
+  Copyright (c) 2020-present André Luiz Alvares
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+]]
+
+local function csv_to_matrix(csv)
+  local matrix = {}
+
+  for line in string.gmatch(csv, "(.-)\n") do
+    table.insert(matrix, {})
+
+    for num in string.gmatch(line, "(-?%d+),?") do
+      local num = tonumber(num)
+      table.insert(matrix[#matrix], num ~= -1)
+    end
+  end
+
+  return matrix
+end
+
+local function matrix_to_regions(matrix)
+  local function mtx_width()
+    local first_line = matrix[1]
+    return #first_line
+  end
+
+  local function mtx_height()
+    return #matrix
+  end
+
+  local function new_region(x, y, w, h)
+    assert(type(x) == 'number')
+    assert(type(y) == 'number')
+    assert(type(w) == 'number')
+    assert(type(h) == 'number')
+    return { x=x, y=y, w=w, h=h }
+  end
+
+  local function new_empty_region()    return new_region(0, 0, 0, 0) end
+  local function new_unit_region(x, y) return new_region(x, y, 1, 1) end
+
+  local regions = {}
+  local current_region = new_empty_region()
+  local prev_cell, next_cell = false, false
+
+  local function reset_state()
+    prev_cell = 0
+    current_region = new_empty_region()
+  end
+
+  local function push_region()
+    if current_region.w > 1 or current_region.h > 1 then
+      table.insert(regions, current_region)
+    end
+
+    reset_state()
+  end
+
+  local function collect_body(x, y, direction)
+    local growing_axis = direction == 'horizontal' and x or y
+    local growing_dimension = direction == 'horizontal' and 'w' or 'h'
+
+    local cell = matrix[y][x]
+    local is_last_cell = x == mtx_width() and y == mtx_height()
+    local is_first_column = growing_axis == 0
+
+    if prev_cell and is_first_column then
+      push_region()
+    end
+
+    if cell then
+      if prev_cell then
+        current_region[growing_dimension] = current_region[growing_dimension] + 1
+        matrix[y][x] = false
+      else
+        current_region = new_region(x, y, 1, 1)
+        if next_cell then
+          matrix[y][x] = false
+        end
+      end
+
+      if is_last_cell then
+        push_region()
+      end
+    else
+      if prev_cell then
+        push_region()
+      end
+    end
+
+    prev_cell = cell
+  end
+
+  for y = 1, mtx_height() do
+    for x = 1, mtx_width() do
+      next_cell = y < mtx_width() and matrix[y][x+1] or false
+      collect_body(x, y, 'horizontal')
+    end
+  end
+
+  reset_state()
+
+  for x = 1, mtx_width() do
+    for y = 1, mtx_height() do
+      next_cell = y < mtx_height() and matrix[y+1][x] or false
+      collect_body(x, y, 'vertical')
+    end
+  end
+
+  for y = 1, mtx_height() do
+    for x = 1, mtx_width() do
+      if matrix[y][x] then
+        table.insert(regions, new_unit_region(x, y))
+      end
+    end
+  end
+
+  return regions
+end
+
+local file <close> = io.open('res/tiled/export.csv')
+local csv = file:read('a')
+local matrix = csv_to_matrix(csv)
+local regions = matrix_to_regions(matrix)
+
+local generated_src = {}
+
+for i, region in ipairs(regions) do
+  local entry = string.format('  { .x = %d, .y = %d, .w = %d, .h = %d }, /* %d */', region.x-1, region.y-1, region.w, region.h, i)
+  table.insert(generated_src, entry)
+end
+
+local header = {
+[[
+#ifndef GGJ_2023_HITBOXES_GUARD
+#define GGJ_2023_HITBOXES_GUARD
+
+#include <genesis.h>
+
+static const Box hitboxes[] = {]],
+
+table.concat(generated_src, '\n'),
+
+[[
+}
+
+#endif /* GGJ_2023_HITBOXES_GUARD */
+]]
+}
+
+local result = table.concat(header, '\n')
+
+local out_file <close> = io.open('res/hitboxes.h', 'w')
+out_file:write(result)
