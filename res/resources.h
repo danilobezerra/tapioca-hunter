5577c828f4d7693446b64d40b79f2f06a767218b
Danilo Bezerra <danilobezerra@outlook.com.br>
2023-02-05 02:08:18 -0300

    Update background music

--- res/resources.h
+++ res/resources.h
@@ -2,8 +2,8 @@
 #define _RES_RESOURCES_H_
 
 extern const u8 main_menu_bgm[3072];
-extern const u8 gameplay_bgm[3072];
-extern const u8 ending_bgm[3072];
+extern const u8 gameplay_bgm[3840];
+extern const u8 ending_bgm[1280];
 extern const Image splash;
 extern const Image title;
 extern const TileSet game_tileset;
